75c17b2eb7dee49bf7e2d065e9c1e328cd2558a7
Induja Vijayaragavan <Induja.Vijayaragavan@mutualofomaha.com>
2016-12-01 15:24:51 -0600

    Added vault installation code

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,48 +1,60 @@
 # Class: vault_config
 # ===========================
 #
-# Full description of class vault_config here.
+# Installs vault and sets up the server configuration to connect to consul secret backend.
 #
 # Parameters
 # ----------
-#
-# Document parameters here.
-#
-# * `sample parameter`
-# Explanation of what this parameter affects and what it defaults to.
-# e.g. "Specify one or more upstream ntp servers as an array."
+# None
 #
 # Variables
 # ----------
-#
-# Here you should define a list of variables that this module would require.
-#
-# * `sample variable`
-#  Explanation of how this variable affects the function of this class and if
-#  it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#  External Node Classifier as a comma separated list of hostnames." (Note,
-#  global variables should be avoided in favor of class parameters as
-#  of Puppet 2.6.)
+# $cdadmin_path, $vault_binary_file
 #
 # Examples
 # --------
 #
 # @example
 #    class { 'vault_config':
-#      servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
 #    }
 #
 # Authors
 # -------
 #
-# Author Name <author@domain.com>
+# Induja Vijayaragavan <induja.vijayaragavan.com>
 #
 # Copyright
 # ---------
 #
-# Copyright 2016 Your name here, unless otherwise noted.
+# Copyright 2016 Induja Vijayaragavan, unless otherwise noted.
 #
 class vault_config {
 
+  require admin_dir_setup
 
+  $cdadmin_path = "/opt/cdadmin/bin"
+
+  $vault_binary_file = "vault_0.6.2_linux_amd64.zip"
+
+  file {"${cdadmin_path}/${vault_binary_file}":
+    ensure => 'present',
+    owner  => 'puppet',
+    group  => 'puppet',
+    source => 'puppet:///modules/vault_config/$vault_binary_file',
+    mode   => '0644',
+  } ->
+
+  exec { 'install_vault':
+    command   => "unzip $cdadmin_path/${vault_binary_file} -d /usr/bin && touch ${cdadmin_path}/one_time_install",
+    subscribe => File[$vault_binary_file],
+    path      => '/usr/bin/:/bin/:/usr/local/bin/',
+    creates   => '${cdadmin_path}/one_time_install',
+  } ->
+  file {"${cdadmin_path}/vault-config.hcl":
+    ensure  => 'present',
+    mode    => '0644',
+    owner   => 'puppet',
+    group   => 'puppet',
+    content => template("vault_config/vault_server_config.erb"),
+  }
 }
